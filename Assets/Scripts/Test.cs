4da4cd4753e2e6cbe725c089102e6d870e729b7a
krikaa <kristjan.kaal@hotmail.com>
2023-03-13 20:08:59 +0200

    Testime Test faili (push3)

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,11 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< HEAD
-using Melanchall.DryWetMidi.Multimedia;
-=======
 using Melanchall.DryWetMidi.Core;
->>>>>>> d77661aeffeb6a2bcd53cf3a3055fd4bd596821f
 
 public class Test : MonoBehaviour
 {
