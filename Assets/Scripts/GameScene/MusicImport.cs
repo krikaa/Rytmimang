f41415fa5df101729396e1b39d9fcce85168f84a
krikaa <kristjan.kaal@hotmail.com>
2023-04-09 22:24:04 +0300

    Helifaili impordi kommentaarid

--- Assets/Scripts/GameScene/MusicImport.cs
+++ Assets/Scripts/GameScene/MusicImport.cs
@@ -7,14 +7,14 @@ public class MusicImport : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip audioClip;
-    public string musicName = "Highscore.mp3";
+    public string musicName = "Highscore.mp3";              //string, mis hoiab imporditava muusikafaili nime
 
     // Start is called before the first frame update
     void Start()
     {
-        string musicPath = "file://" + Application.streamingAssetsPath + "/Import/" + musicName;
-        audioSource = gameObject.GetComponent<AudioSource>();
-        StartCoroutine(GetAudioClip(musicPath));
+        string musicPath = "file://" + Application.streamingAssetsPath + "/Import/" + musicName;    //string, mis hoiab m2ngu kaustades streamingAssets/Import/ asuva muusikafaili asukohta
+        audioSource = gameObject.GetComponent<AudioSource>();                                       //v6tab preaguse objekti komponendi andmed
+        StartCoroutine(GetAudioClip(musicPath));                                                    //alustab GetAudioClip coroutine-i v6ttes kaasa faili asukoha andmed (coroutine jooksutab funktsiooni mitme kaadri jooksul), vajalik IEnumeratori jaoks
     }
 
     // Update is called once per frame
@@ -23,22 +23,23 @@ public class MusicImport : MonoBehaviour
         
     }
 
-    IEnumerator GetAudioClip(string musicPath)
+    IEnumerator GetAudioClip(string musicPath)                                                              //IEnumerator - return tyybiga "funktsioon", mis j2tab eelmise returni meelde
     {
-        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(musicPath, AudioType.MPEG))
+        using (UnityWebRequest musicRequest = UnityWebRequestMultimedia.GetAudioClip(musicPath, AudioType.MPEG))     //muutuja, millele m22ratakse funktsioon, millega Unity yritab hankida kindlast kaustast mp2/mp3 tyypi faili
         {
-            yield return www.SendWebRequest();
+            yield return musicRequest.SendWebRequest();                                                              //saadab failip2ringu, j2tkab muuga kuni tuleb yks allolevatest vastustest
 
-            if (www.result == UnityWebRequest.Result.ConnectionError)
+            if (musicRequest.result == UnityWebRequest.Result.ConnectionError)                                       //kui p2ring eba6nnestus
             {
-                Debug.Log(www.error);
+                Debug.Log(musicRequest.error);                                                                       
             }
-            else
+            else                                                                                            //kui p2ring 6nnestus
             {
-                audioClip = DownloadHandlerAudioClip.GetContent(www);
-                audioClip.name = "importedMusic";
-                audioSource.clip = audioClip;
+                audioClip = DownloadHandlerAudioClip.GetContent(musicRequest);                                       //salvestab saadud helifaili heliklipina
+                audioClip.name = "importedMusic";                                                           //paneb heliklipile nime
+                audioSource.clip = audioClip;                                                               //m22rab saadud heliklipi praeguse objekti audioallika heliklipiks
             }
         }
     }
 }
+
