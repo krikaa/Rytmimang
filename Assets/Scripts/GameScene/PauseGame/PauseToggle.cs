c8db530b10a3e7e724f4ccf62d64d39b728795bc
krikaa <kristjan.kaal@hotmail.com>
2023-04-29 23:04:01 +0300

    Level1 + "pause" menüü fix

--- Assets/Scripts/GameScene/PauseGame/PauseToggle.cs
+++ Assets/Scripts/GameScene/PauseGame/PauseToggle.cs
@@ -8,13 +8,22 @@ public class PauseToggle : MonoBehaviour
     public AudioSource gameAudio;
     public GameObject pauseUI;
     public GameObject scoreboard;
+    public bool pauseDoubleCheck = false;
 
     void Update()
     {
+        if (pauseDoubleCheck == true)
+        {
+            gameAudio.Pause();
+            Time.timeScale = 0;
+            pauseUI.SetActive(true);
+            pauseDoubleCheck = false;
+        }
         if (Input.GetKeyDown(keyBind) && !scoreboard.activeInHierarchy)
         {
             if (Time.timeScale == 1)
             {
+                pauseDoubleCheck = true;
                 gameAudio.Pause();
                 Time.timeScale = 0;
                 pauseUI.SetActive(true);
